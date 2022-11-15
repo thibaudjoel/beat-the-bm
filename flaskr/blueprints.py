f7d799154eeb3896a55c5032c9c5c56e20aba740
Thibaud Joel Hadamczik <thadamczik@gmail.com>
2022-11-15 01:44:38 +0100

    modified:   flaskr/blueprints.py 	modified:   flaskr/models.py

--- flaskr/blueprints.py
+++ flaskr/blueprints.py
@@ -32,7 +32,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('auth.login'))
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('auth.index'))
     return render_template('login.html', title='Sign In', form=form)
